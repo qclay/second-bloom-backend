3ab0ac40c656273fd5bf02a35068d59721de39ba
arm4xlll <wormax36@gmail.com>
2026-03-16 12:39:26 +0200

    feat: fix product module

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,16 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductRepository } from './repositories/product.repository';
 import { CategoryModule } from '../category/category.module';
-import { AuctionSharedModule } from '../auction/auction-shared.module';
+import { AuctionModule } from '../auction/auction.module';
 import { TranslationModule } from '../translation/translation.module';
 import { ConversationModule } from '../conversation/conversation.module';
 
 @Module({
   imports: [
     CategoryModule,
-    AuctionSharedModule,
+    forwardRef(() => AuctionModule),
     TranslationModule,
     ConversationModule,
   ],
